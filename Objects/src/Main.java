142b1f819db158d4ce023918d024bf19100a99c4
Osamah <alghammariosamah@gmail.com>
2016-11-03 22:04:05 +0100

    main changed

--- Objects/src/Main.java
+++ Objects/src/Main.java
@@ -5,21 +5,22 @@ public class Main {
     public static void main(String[] args){
         Time t = new Time(18, 30, 50);
         Date d = new Date();
-        d.set(1992,12,11);
-        Date idk = new Date();
-        idk.set(1992,12,11);
-        System.out.println(d);
-        System.out.println(d.equals(idk));// this is the same address memory 
-        System.out.println(d.equals(d));
-
-        System.out.println(d.equals1(idk));
-        System.out.println(d.equals1(d));
-
-        System.out.println(t);
-        Time t1 = new Time();
-        t1.set(4000);
-        System.out.println(t1);
-        System.out.println(t.equals1(t1));
+        
+//        d.set(1992,12,11);
+//        Date idk = new Date();
+//        idk.set(1992,12,11);
+//        System.out.println(d);
+//        System.out.println(d.equals(idk));// this is the same address memory 
+//        System.out.println(d.equals(d));
+//
+//        System.out.println(d.equals1(idk));
+//        System.out.println(d.equals1(d));
+//
+//        System.out.println(t);
+//        Time t1 = new Time();
+//        t1.set(4000);
+//        System.out.println(t1);
+//        System.out.println(t.equals1(t1));
 
 //        d.addMonth(4);
 //        System.out.println(d);
