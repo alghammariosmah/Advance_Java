90a7c0d71830b1e3008890cbe2a57814437150c7
Osamah <alghammariosamah@gmail.com>
2016-11-03 22:32:38 +0100

    Still Mode needs to be adjusted

--- Objects/src/Main.java
+++ Objects/src/Main.java
@@ -6,29 +6,31 @@
 public class Main {
     public static void main(String[] args){
         Time t = new Time(18, 30, 50);
-        Date d = new Date();
-        
+        System.out.println(t);
         t.set(5000);
         System.out.println(t);
-//        d.set(1992,12,11);
-//        Date idk = new Date();
-//        idk.set(1992,12,11);
-//        System.out.println(d);
-//        System.out.println(d.equals(idk));// this is the same address memory 
-//        System.out.println(d.equals(d));
-//
-//        System.out.println(d.equals1(idk));
-//        System.out.println(d.equals1(d));
-//
-//        System.out.println(t);
-//        Time t1 = new Time();
-//        t1.set(4000);
-//        System.out.println(t1);
-//        System.out.println(t.equals1(t1));
+        t.addSec(100);
+        System.out.println(t);
+        t.addMin(100);
+        System.out.println(t);
+        t.addHour(3);
+        System.out.println(t);
+        Time test = new Time();
+        test.set(5000);
+        System.out.println(t.equals1(test));
 
-//        d.addMonth(4);
-//        System.out.println(d);
-//        d.addYear(1000000);
-//        System.out.println(d);
+        Date d = new Date();
+        System.out.println(d);
+        d.set(1992, 12, 2);
+        System.out.println(d);
+        d.add(12);
+        System.out.println(d);
+        d.addMonth(4);
+        System.out.println(d);
+        d.addYear(1000000);
+        System.out.println(d);
+        Date test1 = new Date();
+        test1.set(1001993,4,14);
+        System.out.println(d.equals(test1));
     }
 }
